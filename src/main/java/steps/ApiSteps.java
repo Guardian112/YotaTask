e3d577713815d9aaca6a111b393e4773c485f2d5
Guardian <antonzelenkov112@gmail.com>
2023-12-21 16:03:09 +0300

    Здарова👋

--- src/main/java/steps/ApiSteps.java
+++ src/main/java/steps/ApiSteps.java
@@ -1,93 +1,93 @@
-package steps.apiSteps;
+package steps;
 
 import io.qameta.allure.Step;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import models.other.*;
-import models.request.*;
 import models.response.*;
+import models.request.*;
 
 import static io.restassured.RestAssured.given;
-public class UserSteps {
-    @Step("Авторизация")
-    public LoginTokenResponse postLoginUser (LoginRequest LogReq) {
+
+public class ApiSteps {
+    @Step ("Авторизация")
+    public LoginTokenResponse postLogin(LoginRequest LogReq, Integer statusCode) {
         return given()
                 .baseUri("http://localhost:8080")
                 .body(LogReq)
                 .contentType(ContentType.JSON)
                 .post("/login")
                 .then()
-                .assertThat()
-                .statusCode(200)
+                .statusCode(statusCode)
                 .extract()
                 .response().as(LoginTokenResponse.class);
     }
     @Step ("Получение списка свободных телефонов")
-    public PhonesResponse getEmptyPhonesUser (String Token) {
+    public PhonesResponse getEmptyPhones(String token, Integer statusCode) {
         return given()
-                .header("authToken", Token)
+                .header("authToken", token)
                 .baseUri("http://localhost:8080")
                 .get("/simcards/getEmptyPhone")
                 .then()
                 .assertThat()
-                .statusCode(200)
+                .statusCode(statusCode)
                 .extract()
                 .response().as(PhonesResponse.class);
     }
     @Step ("Создание нового кастомера")
-    public IdResponse postCreateCustomerUser(String Token, CustomerRequest CastReq) {
+    public IdResponse postCreateCustomer(String token, CustomerRequest CastReq, Integer statusCode) {
         return given()
-                .header("authToken", Token)
+                .header("authToken", token)
                 .baseUri("http://localhost:8080")
                 .body(CastReq)
                 .contentType(ContentType.JSON)
                 .post("/customer/postCustomer")
                 .then()
-                .statusCode(200)
+                .statusCode(statusCode)
                 .extract()
                 .response().as(IdResponse.class);
     }
     @Step ("Проверка корректности активации кастомера")
-    public CustomerById getCustomerByIdUser(String Token, String customerId) {
+    public CustomerById getCustomerById(String token, String customerId, Integer statusCode) {
         return given()
-                .header("authToken", Token)
+                .header("authToken", token)
                 .baseUri("http://localhost:8080")
                 .get("/customer/getCustomerById" + "?customerId=" + customerId)
                 .then()
-                .statusCode(200)
+                .statusCode(statusCode)
                 .extract()
                 .response().as(CustomerById.class);
     }
     @Step ("Проверка что кастомер сохранился в старой системе")
-    public Response postCustomerByPhoneUser(String Token, Long Number) {
+    public Response postCustomerByPhone(String token, Long number, Integer statusCode) {
         return given()
                 .baseUri("http://localhost:8080")
                 .body("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
                         "<ns3:Envelope xmlns:ns2=\"soap\" xmlns:ns3=\"http://schemas.xmlsoap.org/soap/envelope\">\n" +
                         "    <ns2:Header>\n" +
-                        "        <authToken>" + Token + "</authToken>\n" +
+                        "        <authToken>" + token + "</authToken>\n" +
                         "    </ns2:Header>\n" +
                         "    <ns2:Body>\n" +
-                        "        <phoneNumber>" + Number + "</phoneNumber>\n" +
+                        "        <phoneNumber>" + number + "</phoneNumber>\n" +
                         "    </ns2:Body>\n" +
                         "</ns3:Envelope>")
                 .contentType(ContentType.XML)
                 .post("/customer/findByPhoneNumber")
                 .then()
-                .statusCode(200)
+                .statusCode(statusCode)
                 .extract()
                 .response();
     }
     @Step ("Смена статуса кастомера")
-    public void postChangeCustomerStatusUser(StatusRequest StReq, String Token, String customerId) {
+    public void postChangeCustomerStatus(StatusRequest StReq, String token, String customerId, Integer statusCode) {
         given()
-                .header("authToken", Token)
+                .header("authToken", token)
                 .baseUri("http://localhost:8080")
                 .body(StReq)
                 .contentType(ContentType.JSON)
                 .post("/customer/" + customerId + "/changeCustomerStatus")
                 .then()
-                .statusCode(401)
+                .statusCode(statusCode)
                 .extract()
                 .response();
     }
--- src/main/java/steps/Steps.java
+++ src/main/java/steps/Steps.java
@@ -1,7 +1,5 @@
 package steps;
-import steps.apiSteps.AdminSteps;
-import steps.apiSteps.UserSteps;
+
 public interface Steps {
-    AdminSteps ADMIN_STEPS = new AdminSteps();
-    UserSteps USER_STEPS = new UserSteps();
+    ApiSteps API_STEPS = new ApiSteps();
 }
