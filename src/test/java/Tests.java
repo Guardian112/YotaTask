55e183fbe5ff42597b72a86cd0b0a95689cbfe8a
Guardian <antonzelenkov112@gmail.com>
2023-12-21 14:29:29 +0300

    Здарова👋

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,66 +8,44 @@ import models.response.*;
 import org.testng.annotations.Test;
 
 public class Tests extends BaseTest {
-    @Test(description = "Пользователь авторизуется в системе под своим логином/паролем")
-    public void Login() {
+    @Test (description = "Проверка сценария активации абонента по шагам из бизнес-сценария")
+    public void ABC() throws JsonProcessingException {
+//--------------------------------------------------------------------------------------------------------------------
+        // Пункт 1 - Пользователь авторизуется в системе под своим логином/паролем
         LoginRequest loginAdmin = new LoginRequest("admin", "password");
         LoginTokenResponse Token = ADMIN_STEPS.postLoginAdm(loginAdmin); // <-- Тут получаем токен
-        System.out.println(Token.getToken());
-    }
-
-    @Test(description = "Пользователь получает список свободных номеров")
-    public void EmptyPhones() {
-        LoginRequest loginAdmin = new LoginRequest("admin", "password");
-        LoginTokenResponse Token = ADMIN_STEPS.postLoginAdm(loginAdmin); // <-- Тут получаем токен
+        System.out.println("Токен: " + Token.getToken());
+//--------------------------------------------------------------------------------------------------------------------
+        // Пункт 2 - Пользователь получает список свободных номеров
         PhonesResponse numberList = ADMIN_STEPS.getEmptyPhonesAdm(Token.getToken(), 200);
-        System.out.println(numberList);
-    }
-
-    // Этот надо повторять до успеха
-
-    @Test(description = "Пользователь создает нового кастомера")
-    public void CreateCustomer() {
-        LoginRequest loginAdmin = new LoginRequest("admin", "password");
-        LoginTokenResponse Token = ADMIN_STEPS.postLoginAdm(loginAdmin); // <-- Тут получаем токен
+        System.out.println("Список номеров: " + numberList);
+//--------------------------------------------------------------------------------------------------------------------
+        // Пункт 3 - Пользователь создает нового кастомера
         AdditionalParameters addPar = new AdditionalParameters("string"); // <-- Тут определяем доп. параметры
         CustomerRequest customer = new CustomerRequest("sdfsdf", 79281326346L, addPar);
         IdResponse customerId = ADMIN_STEPS.postCreateCustomerAdm(Token.getToken(), customer); // <-- Тут получаем id
-        System.out.println(customerId.getId());
+        System.out.println("Id нового пользователя: " + customerId.getId());
+//--------------------------------------------------------------------------------------------------------------------
+        // Пункт 4 - В промежуток времени до 2х минут кастомер активируется
         try {
-            Thread.sleep(120000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @Test(description = "Пользователь проверяет корректность активации кастомера")
-    public void CustomerById() {
-        LoginRequest loginAdmin = new LoginRequest("admin", "password");
-        LoginTokenResponse Token = ADMIN_STEPS.postLoginAdm(loginAdmin); // <-- Тут получаем токен
-        AdditionalParameters addPar = new AdditionalParameters("string"); // <-- Тут определяем доп. параметры
-        CustomerRequest customer = new CustomerRequest("sdfsdf", 79281326346L, addPar);
-        IdResponse customerId = ADMIN_STEPS.postCreateCustomerAdm(Token.getToken(), customer); //<-- Тут получаем id
+//--------------------------------------------------------------------------------------------------------------------
+        // Пункт 5 - пользователь проверяет корректность активации кастомера
         CustomerById CustomerById = ADMIN_STEPS.getCustomerByIdAdm(Token.getToken(), customerId.getId());
-        System.out.println(CustomerById);
-    }
-
-    @Test(description = "Пользователь проверяет, что кастомер сохранился в старой системе")
-    public void CustomerByPhone() throws JsonProcessingException {
-        LoginRequest loginAdmin = new LoginRequest("admin", "password");
-        LoginTokenResponse Token = ADMIN_STEPS.postLoginAdm(loginAdmin); // <-- Тут получаем токен
+        System.out.println("Информация о пользователе: " + CustomerById);
+//--------------------------------------------------------------------------------------------------------------------
+        // Пункт 6 - Пользователь проверяет, что кастомер сохранился в старой системе
         Response IdByPhone = ADMIN_STEPS.postCustomerByPhoneAdm(Token.getToken(), 79281326346L);
         FindByPhoneResponse name = new XmlMapper().readValue(IdByPhone.getBody().asString(),FindByPhoneResponse.class);
-        System.out.println(name);
-    }
-
-    @Test(description = "Пользователь меняет кастомеру статус")
-    public void ChangeCustomerStatus() {
-        LoginRequest loginAdmin = new LoginRequest("admin", "password");
-        LoginTokenResponse Token = ADMIN_STEPS.postLoginAdm(loginAdmin); // <-- Тут получаем токен
-        AdditionalParameters addPar = new AdditionalParameters("string"); // <-- Тут определяем доп. параметры
-        CustomerRequest customer = new CustomerRequest("sdfsdf", 79281326346L, addPar);
+        System.out.println("Проверка сохранения пользователя (xml): " + name);
+//--------------------------------------------------------------------------------------------------------------------
+        // Пункт 7 - Пользователь admin изменяет кастомеру статус
         StatusRequest stat = new StatusRequest("ACTIVE");
-        IdResponse customerId = ADMIN_STEPS.postCreateCustomerAdm(Token.getToken(), customer); //<-- Тут получаем id
         ADMIN_STEPS.postChangeCustomerStatusAdm(stat, Token.getToken(), customerId.getId());
+        System.out.println("Статус пользователя изменён на ACTIVE");
+//--------------------------------------------------------------------------------------------------------------------
     }
 }
