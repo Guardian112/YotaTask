95f45d6804b56de3d0deb2fa531b8ffa289ee062
Guardian <antonzelenkov112@gmail.com>
2023-12-21 16:57:52 +0300

    EEEEEE

--- src/test/java/AdminTests.java
+++ src/test/java/AdminTests.java
@@ -9,45 +9,43 @@ import org.testng.annotations.Test;
 
 public class AdminTests extends BaseTest {
     @Test (description = "Проверка сценария активации абонента по шагам из бизнес-сценария за Админа")
-    public void TestAdmin() throws JsonProcessingException {
+    public void TestAdmin() throws JsonProcessingException, InterruptedException {
         // Пункт 1 - Пользователь авторизуется в системе под своим логином/паролем
         LoginRequest loginAdmin = new LoginRequest("admin", "password");
         LoginTokenResponse token = API_STEPS.postLogin(loginAdmin, 200); // <-- Тут получаем токен
-        System.out.println("Токен: " + token.getToken());
+        System.out.println("Токен: " + token.getToken() + "\n");
 
         // Пункт 2 - Пользователь получает список свободных номеров
         PhonesResponse numberList = API_STEPS.getEmptyPhones(token.getToken(), 200);
-        System.out.println("Список номеров: " + numberList);
+        System.out.println("Список номеров: " + numberList + "\n");
 
         // Пункт 3 - Пользователь создает нового кастомера
         AdditionalParameters addPar = new AdditionalParameters("string"); // <-- Тут определяем доп. параметры
         CustomerRequest customer = new CustomerRequest("sdfsdf", 79281326346L, addPar);
         IdResponse customerId = API_STEPS.postCreateCustomer(token.getToken(), customer, 200); // <-- Тут получаем id
-        System.out.println("Id нового пользователя: " + customerId.getId());
+        System.out.println("Id нового пользователя: " + customerId.getId() + "\n");
 
         // Пункт 4 - В промежуток времени до 2х минут кастомер активируется
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Thread.sleep(120000);
 
         // Пункт 5 - пользователь проверяет корректность активации кастомера
         CustomerById CustomerById = API_STEPS.getCustomerById(token.getToken(), customerId.getId(), 200);
-        System.out.println("Информация о пользователе: " + CustomerById);
+        API_STEPS.checkEqualResponse("ACTIVE", CustomerById.getMyreturn().getStatus());
+        System.out.println("Информация о пользователе: " + CustomerById + "\n");
 
         // Пункт 6 - Пользователь проверяет, что кастомер сохранился в старой системе
         Response IdByPhone = API_STEPS.postCustomerByPhone(token.getToken(), 79281326346L, 200);
         FindByPhoneResponse name = new XmlMapper().readValue(IdByPhone.getBody().asString(),FindByPhoneResponse.class);
-        API_STEPS.checkEqualResponse(customerId.getId(), name.getBodyResponse().getCustomerId());
-        System.out.println("Проверка сохранения пользователя (xml): " + name);
+        System.out.println("Проверка сохранения пользователя (xml): " + name + "\n");
 
         // Пункт 7 - Пользователь admin изменяет кастомеру статус
-        StatusRequest stat = new StatusRequest("ACTIVE");
+        StatusRequest stat = new StatusRequest("BOSS");
         API_STEPS.postChangeCustomerStatus(stat, token.getToken(), customerId.getId(), 200);
-        System.out.println("Статус пользователя изменён на ACTIVE");
+        System.out.println("Статус пользователя изменён на BOSS");
+
         // Пункт 8 - Пользователь проверяет что изменения прошли успешно
         CustomerById NewCustomerById = API_STEPS.getCustomerById(token.getToken(), customerId.getId(), 200);
+        API_STEPS.checkEqualResponse("BOSS", name.getBodyResponse().getCustomerId());
         System.out.println("Статус успешно изменён на: " + NewCustomerById.getMyreturn().getStatus());
     }
 }
--- src/test/java/UserTests.java
+++ src/test/java/UserTests.java
@@ -6,39 +6,37 @@ import steps.*;
 import models.request.*;
 import models.response.*;
 import org.testng.annotations.Test;
+
 public class UserTests extends BaseTest {
     @Test (description = "Проверка сценария активации абонента по шагам из бизнес-сценария за Юзера")
-    public void TestUser() throws JsonProcessingException {
+    public void TestUser() throws JsonProcessingException, InterruptedException {
         // Пункт 1 - Пользователь авторизуется в системе под своим логином/паролем
         LoginRequest loginAdmin = new LoginRequest("user", "password");
         LoginTokenResponse Token = API_STEPS.postLogin(loginAdmin, 200); // <-- Тут получаем токен
-        System.out.println("Токен: " + Token.getToken());
+        System.out.println("Токен: " + Token.getToken() + "\n");
 
         // Пункт 2 - Пользователь получает список свободных номеров
         PhonesResponse numberList = API_STEPS.getEmptyPhones(Token.getToken(), 200);
-        System.out.println("Список номеров: " + numberList);
+        System.out.println("Список номеров: " + numberList + "\n");
 
         // Пункт 3 - Пользователь создает нового кастомера
         AdditionalParameters addPar = new AdditionalParameters("string"); // <-- Тут определяем доп. параметры
         CustomerRequest customer = new CustomerRequest("sdfsdf", 79281326346L, addPar);
         IdResponse customerId = API_STEPS.postCreateCustomer(Token.getToken(), customer, 200); // <-- Тут получаем id
-        System.out.println("Id нового пользователя: " + customerId.getId());
+        System.out.println("Id нового пользователя: " + customerId.getId() + "\n");
 
         // Пункт 4 - В промежуток времени до 2х минут кастомер активируется
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Thread.sleep(120000);
 
         // Пункт 5 - пользователь проверяет корректность активации кастомера
         CustomerById CustomerById = API_STEPS.getCustomerById(Token.getToken(), customerId.getId(), 200);
-        System.out.println("Информация о пользователе: " + CustomerById);
+        API_STEPS.checkEqualResponse("ACTIVE", CustomerById.getMyreturn().getStatus());
+        System.out.println("Информация о пользователе: " + CustomerById + "\n");
 
         // Пункт 6 - Пользователь проверяет, что кастомер сохранился в старой системе
         Response IdByPhone = API_STEPS.postCustomerByPhone(Token.getToken(), 79281326346L, 200);
         FindByPhoneResponse name = new XmlMapper().readValue(IdByPhone.getBody().asString(),FindByPhoneResponse.class);
-        System.out.println("Проверка сохранения пользователя (xml): " + name);
+        System.out.println("Проверка сохранения пользователя (xml): " + name + "\n");
 
         // Пункт 7 - Пользователь admin изменяет кастомеру статус
         StatusRequest stat = new StatusRequest("ACTIVE");
